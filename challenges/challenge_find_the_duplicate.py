1526a24aa69bac3af5ed152b4c84357c19a8b51a
Ester Viana Verza <ester.av@hotmail.com>
2023-08-12 18:31:22 -0500

    feat:find the duplicate

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,32 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    check_array(nums)
+    dup_numbers = set()
+    """o set cria um dicionario ordenado"""
+    """Returns : An empty set if no element is passed.
+    --> Non-repeating element iterable modified as passed as argument"""
+    if not check_number(nums):
+        return False
+    for number in nums:
+        if number in dup_numbers:
+            return number
+        else:
+            dup_numbers.add(number)
+    return False
+
+
+def check_array(nums):
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
+
+
+def check_number(nums):
+    try:
+        for num in nums:
+            if num < 0 or type(num) != int:
+                return False
+        return True
+    except TypeError:
+        return False
+
+
+""" raise NotImplementedError """
