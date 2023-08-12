82a2b6ea536033a7e2f55595bbd0e2b0bc62b91a
Ester Viana Verza <ester.av@hotmail.com>
2023-08-12 17:32:32 -0500

    feat: testes encrypt

--- challenges/challenge_encrypt_message.py
+++ challenges/challenge_encrypt_message.py
@@ -1,5 +1,4 @@
 def encrypt_message(message: str, key: int):
-
     if not isinstance(key, int):
         raise TypeError("tipo inválido para key")
 
@@ -16,3 +15,15 @@ def encrypt_message(message: str, key: int):
         part_two, part_one = part_one, part_two
 
     return "".join(part_one) + "_" + "".join(part_two)
+
+
+""" Recebe uma string message e um inteiro key como parâmetros
+Se key e message não possuírem os tipos corretos, uma exceção deve ser lançada
+Se key não for um índice positivo válido de message,
+retorna a string message invertida
+Se key for ímpar:
+divide message no índice key, inverte os caracteres de cada parte, e retorna
+a união das partes novamente com "_" entre elas
+Se key for par:
+divide message no índice key, inverte a posição das partes, inverte os
+caracteres de cada parte, e retorna a união das partes com "_" entre elas """
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,23 @@
 # from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
     pass
+    text = 'abobora'
+    all_reversed_text = 'aroboba'
+    three_reversed_text = 'oba_arob'
+    two_reversed_text = 'arobo_ba'
+
+    assert encrypt_message("", 2) == ""
+    assert encrypt_message(text, 0) == all_reversed_text
+    assert encrypt_message(text, 10) == all_reversed_text
+    assert encrypt_message(text, -10) == all_reversed_text
+    assert encrypt_message(text, 7) == all_reversed_text
+    assert encrypt_message(text, 3) == three_reversed_text
+    assert encrypt_message(text, 2) == two_reversed_text
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(text, "a")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 10)
