545030490c111b0f587432a01e39f19b4d473ba3
Ester Viana Verza <ester.av@hotmail.com>
2023-08-12 16:56:27 -0500

    feat: challenge_study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
+    if not isinstance(target_time, int) or not isinstance(
+        permanence_period, list
+    ):
+        return None
+    period = 0
+    for p_start, p_end in permanence_period:
+        try:
+            if p_start <= int(target_time) <= p_end:
+                period += 1
+        except TypeError:
+            return None
+    return period
     raise NotImplementedError
