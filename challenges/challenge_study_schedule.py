c506c90ebf74a9916891c360d346a21e141eab31
Ester Viana Verza <ester.av@hotmail.com>
2023-08-12 17:06:27 -0500

    fix: study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,14 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    if not isinstance(target_time, int) or not isinstance(
-        permanence_period, list
-    ):
-        return None
-    period = 0
-    for p_start, p_end in permanence_period:
-        try:
+    try:
+        period = 0
+        for p_start, p_end in permanence_period:
             if p_start <= int(target_time) <= p_end:
                 period += 1
-        except TypeError:
-            return None
-    return period
-    raise NotImplementedError
+        return period
+    except TypeError:
+        return None
+
+
+"""     raise NotImplementedError """
