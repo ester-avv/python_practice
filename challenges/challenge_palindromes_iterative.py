043303f707084027f81bdf033d1743d561aa0d52
Ester Viana Verza <ester.av@hotmail.com>
2023-08-12 19:47:55 -0500

    feat palindromes interative

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,12 @@
-def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+def is_palindrome_iterative(word: str):
+    size = len(word)
+    if size == 0:
+        return False
+
+    # no loop compara o elem no index i com a posição contraria
+    # se for igual e palind
+    for i in range(size // 2):
+        if word[i] != word[size - 1 - i]:
+            return False
+    return True
+    """ raise NotImplementedError """
